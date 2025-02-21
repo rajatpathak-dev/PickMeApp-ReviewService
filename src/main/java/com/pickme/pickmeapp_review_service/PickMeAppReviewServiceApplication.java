96fb20d7a17339ec7b92c4e8079a4d5b7083f8bc
Rajat Pathak <rajatpathak9888@gmail.com>
2025-02-21 19:48:15 +0530

    adding empty project on github

---
+++ src/main/java/com/pickme/pickmeapp_review_service/PickMeAppReviewServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pickme.pickmeapp_review_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickMeAppReviewServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PickMeAppReviewServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pickme/pickmeapp_review_service/PickMeAppReviewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pickme.pickmeapp_review_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickMeAppReviewServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
